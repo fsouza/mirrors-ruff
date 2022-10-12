e25e3632dc94f7ef807809fbe89ce8eccbe3eb49
francisco souza <108725+fsouza@users.noreply.github.com>
2022-10-12 09:41:36 -0400

    scripts/update-readme: fix sed and amend commit

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 if git describe --tags --exact-match HEAD &>/dev/null; then
 	tag=$(git describe --tags --exact-match HEAD)
 	old_sha=$(shasum -a 256 README.md)
-	sed -i 's/rev: .\+/rev: '"${tag}" README.md
+	sed -i 's/rev: .\+/rev: '"${tag}/" README.md
 	new_sha=$(shasum -a 256 README.md)
 
 	if [[ ${new_sha} != "${old_sha}" ]]; then
-		git commit -m "Update README" README.md
+		git commit --amend --no-edit README.md
 	fi
 fi
